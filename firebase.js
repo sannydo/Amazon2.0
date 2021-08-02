3278c1c9a06b3422ce3219fb43899c3337ac2515
sanny do <sannydo94@gmail.com>
2021-08-01 19:32:03 -0500

    amazon

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// var firebaseConfig = {
+//     apiKey: "AIzaSyDIZ5_HLRJ6aJ4xj5YWmIQt0Nq92ZtVIyU",
+//     authDomain: "clone-two-151fb.firebaseapp.com",
+//     projectId: "clone-two-151fb",
+//     storageBucket: "clone-two-151fb.appspot.com",
+//     messagingSenderId: "638594283465",
+//     appId: "1:638594283465:web:9d3b388020d560ba051a18",
+//     measurementId: "G-2BCWNHCTG6"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
---
+++ script.js
@@ -0,0 +1,15 @@
+// addToCartEl.classList.add("hover:bg-yellow-600", "cursor-pointer", "product-add", "h-8", "w-28", "rounded", "bg-yellow-500", "text-white", "text-md", "flex", "justify-center", "items-center");
+
+// we want to show cart items
+
+// function getCartItems(){
+//     db.collection("cart-items").onSnapshot((snapshot) =>{
+//         snapshot.docs.forEach((doc) => {
+//             items.push({
+//                 id: doc.id,
+//                 ...doc.data()
+//             })
+//         })
+//         generateCartItems(cartItems);
+//     })
+// }
